c38de848aa1eb555ff769bc3660125d5c3aab523
awaxman11 <awaxman11@gmail.com>
2013-06-25 13:33:02 -0400

    First adam commit

--- app.rb
+++ app.rb
@@ -1,10 +1,25 @@
 require 'sinatra/base'
+require './student.rb'
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
   class App < Sinatra::Base
     get '/' do
       "hello world!"
+    end
+
+    get '/hello-world' do 
+      erb :hello
+    end
+
+    get '/students' do 
+      @students = Student.all
+      erb :'students/index'
+    end
+
+    get '/students/:id' do 
+      @student = Student.find(params[:id])
+      erb :'students/template'
     end
   end
 end
